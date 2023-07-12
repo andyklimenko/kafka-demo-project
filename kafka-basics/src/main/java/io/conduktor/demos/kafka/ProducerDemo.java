efdeb15149e955500fd195d22ac2b10364370e92
Andrii Klymenko <andrii.klymenko@underdefense.com>
2023-07-12 15:01:12 +0200

    one message producer added

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemo.java
@@ -1,7 +1,35 @@
 package io.conduktor.demos.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
 public class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
     public static void main(String[] args) {
-         System.out.println("Hello world");
+         log.info("Hello world");
+
+         // create producer properties
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:29092");
+
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer(properties);
+
+        ProducerRecord<String, String> producerRecord = new ProducerRecord("demo_java", "hello world");
+        producer.send(producerRecord);
+
+        // producer will send all data and block until done - synchronous operation
+        producer.flush();
+
+        producer.close();
     }
 }
