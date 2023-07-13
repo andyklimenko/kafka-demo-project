2ad7176dff951ae6f85b5cd4f1b404a93465beb2
Andrii Klymenko <andrii.klymenko@underdefense.com>
2023-07-13 15:38:14 +0200

    added consumer with graceful shutdown

--- README.md
+++ README.md
@@ -24,3 +24,4 @@ kafka-console-consumer.sh --bootstrap-server localhost:29092 --topic demo_java -
 3. [Producer with callback with no sticky partitioning](./kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerWithCallbackNoStickyPartitioner.java)
 4. [Producer with message keys](./kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerWithKeys.java)
 5. [Consumer demo](./kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerDemo.java)
+6. [Consumer demo with graceful shutdown](./kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerWithGracefulShutdown.java)
---
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerWithGracefulShutdown.java
@@ -0,0 +1,76 @@
+package io.conduktor.demos.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerWithGracefulShutdown {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        final String groupID = "mu-java-application";
+        final String topic = "demo_java";
+
+        log.info("I'm a Kafka Consumer!");
+
+        // create producer properties
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:29092");
+
+        // create consumer configs
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+
+        properties.setProperty("group.id", groupID);
+
+        // supported values - [none | earliest | latest]
+        // none     - if we don't have any existing consumer group - we fail
+        // earliest - read from the beginning of the topic(similar to --from-beginning option passed to kafka-console-consumer.sh
+        // latest   - we want to read only new messages
+        properties.setProperty("auto.offset.reset", "earliest");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        final Thread mainThread = Thread.currentThread();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                log.info("Detected a shutdown, let's exit..");
+                consumer.wakeup(); // will make a consumer to throw a WakeupException
+
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        consumer.subscribe(Arrays.asList(topic));
+
+        try {
+            while (true) {
+                log.info("Polling..");
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+                for (ConsumerRecord<String, String> r : records) {
+                    log.info("Key: " + r.key() + ", Value: " + r.value());
+                    log.info("Partition: " + r.partition() + " Offset: " + r.offset());
+                }
+            }
+        } catch (WakeupException e) {
+            log.info("consumer is starting to shutdown");
+        } catch (Exception e) {
+            log.error("unexpected exception", e);
+        } finally {
+            consumer.close(); // also commits the offsets
+            log.info("the consumer is now gracefully shutdown");
+        }
+    }
+}
